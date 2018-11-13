385ed28b588da8cecbd76a4655474440dac1699e
CoreyY22 <44762550+CoreyY22@users.noreply.github.com>
2018-11-13 16:50:32 -0400

    Add files via upload

---
+++ admin/accommodation.php
@@ -0,0 +1,198 @@
+<html>
+<head>
+	<title>Artifact View</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<style>
+	
+	 body {
+  margin: 0;
+  padding: 0;
+  background: #ccc;
+  background-image: url(image/);
+  background-repeat: no-repeat;
+  background-size: 100% 3550px;
+}
+
+#rtable {
+    border-radius: 25px;
+    border: 2px solid #;
+    padding: 5px; 
+    width: 200px;
+    height: 150px;    
+}
+	
+	 </style>
+</head>
+
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="index.php">Museum Homepage</a>
+	    </div>
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li><a href=home.php>Home</a></li>
+	        <li><a href=accommodation.php>View Artifact</a></li>
+					<li><a href=tariff.php>TBD</a></li>
+					<li><a href=aboutus.php>About Us</a></li>
+	      </ul>
+				<ul class="nav navbar-nav navbar-right">
+        <li><a href="login.php">Login</a></li>
+      </ul>
+	    </div>
+	  </div>
+	</nav>
+<br>
+<br>
+<img src="image/TVlogo.jpg" width="100%" height="100" alt>
+<table width="970" border="0" cellpadding="0" cellspacing"0" align="center">
+<tbody>
+<tr>
+<td width="970" height="27" valign="top">
+<div align="center" style="margin-top:5px">
+<table width="970" border="0" cellpadding="0" cellspacing="0">
+<tbody>
+<tr>
+<td>
+
+</td>
+</tr>
+</td>
+</tr>
+  
+  <table style="width:100%;" border="0">
+  
+<center>
+   <h1><p style="font-family:georgia,garamond,serif;color: white;">Museum Information</p></h1>
+   </center>
+
+//<p style="font-family:georgia,garamond,serif;color: white;"> Mabaruma is a small village located in Region #1, Barima Waini, known to many as North West District. </p>
+
+<center>
+   <table style="width:100%;" border="0">
+	<tr>
+		<td id="rtable"><img src="image/mabaruma1.jpg" width="300" height="250" alt><td>
+		<td id="rtable"><img src="image/mabaruma2.jpg" width="300" height="250" alt></td>
+		<td id="rtable"><img src="image/mabaruma6.jpg" width="300" height="250" alt></td>
+	</tr>
+	</table>
+	
+<p style="font-family:georgia,garamond,serif;color: white;"> It is also one of the most populated villages in Guyana and has over the years attracted a large number of tourist mainly because of the breath taking view within the small village. Upon entering Mabaruma you will be greeted with one of the most beautiful scenes found in this small area.</p>
+<br>
+
+	<table style="width:100%;" border="0">
+	<tr>
+		<td id="rtable"><img src="image/flowers1.jpg" width="300" height="250" alt><td>
+		<td id="rtable"><img src="image/flowers2.jpg" width="300" height="250" alt></td>
+		<td id="rtable"><img src="image/flowers3.jpg" width="300" height="250" alt></td>
+	</tr>
+</table>
+
+<p style="font-family:georgia,garamond,serif;color: white;">This small village is also well known for its large scale of fauna and flora which can be seen almost everywhere you go. Mabaruma is also very close to the Venezuela borders and offers hotels with breath taking view to these visitors. Tourist also enjoys the tradition meals prepared by our catering service.<p>
+
+ <table style="width:100%;" border="0">
+<tr>
+   <center>
+   <h1><p style="font-family:georgia,garamond,serif;color: white;">South Ruimveldt</p></h1>
+   </center>
+   </header>
+   <center>
+   <p style="font-family:georgia,garamond,serif;color: white;"> South Ruimveldt is a community that is located in Georgetown, Guyana. It is divided into two main parts, namely South Ruimveldt Park and South Ruimveldt Gardens. South Ruimveldt is a part of the Ruimveldt Estate which used to play in major role in the cultivation and production of sugar.</p>
+   <br>
+   <p style="font-family:georgia,garamond,serif;color: white;"> South Ruimveldt is an industrialized community which has great places to visit, to eat, sit back and relax. Its an ideal area to get away from the busy city life.</p>
+   </center>
+   <br>
+   <center>
+   <table style="width:100%;" border="0">
+	<tr>
+		<td id="rtable"><img src="image/plants4.jpg" width="300" height="250" alt><td>
+		<td id="rtable"><img src="image/plants3.jpg" width="300" height="250" alt></td>
+		<td id="rtable"><img src="image/plants2.jpg" width="300" height="250" alt></td>
+	</tr>
+	<table style="width:100%;" border="0">
+	<tr>
+		<td id="rtable"><img src="image/south.jpg" width="300" height="250" alt><td>
+		<td id="rtable"><img src="image/plant31.jpg" width="300" height="250" alt></td>
+		<td id="rtable"><img src="image/plant30.jpg" width="300" height="250" alt></td>
+	</tr>
+	</table>
+	<p style="font-family:georgia,garamond,serif; color: white;">This community is well known for its large scale of fauna and flora which can be seen almost everywhere in South Ruimveldt.<p>
+	<table style="width:100%;" border="0">
+	<tr>
+		<td id="rtable"><img src="image/mangoes.jpg" width="300" height="250" alt><td>
+		<td id="rtable"><img src="image/scene2.jpg" width="300" height="250" alt></td>
+		<td id="rtable"><img src="image/plants3.jpg" width="300" height="250" alt></td>
+	</tr>
+	</table>
+	<table style="width:100%;" border="0">
+	<tr>
+		<td id="rtable"><img src="image/plants.jpg" width="300" height="250" alt><td>
+		<td id="rtable"><img src="image/oranges.jpg" width="300" height="250" alt></td>
+		<td id="rtable"><img src="image/southno.jpg" width="300" height="250" alt></td>
+	</tr>
+	</table>
+	<table style="width:100%;" border="0">
+  
+<center>
+   <h1><p style="font-family:georgia,garamond,serif;color: white;">Kuru Kururu</p></h1>
+   </center>
+
+<p style="font-family:georgia,garamond,serif;color: white;"> Kuru Kururu is a settlement located on the Soesdyke-Linden highway in the hilly sand and clay region. Approximately three thousand and eighty persons reside in this village, which consist mostly of persons of mixed culture. </p>
+
+<center>
+   <table style="width:100%;" border="0">
+	<tr>
+		<td id="rtable"><img src="image/view2.jpg" width="300" height="250" alt><td>
+		<td id="rtable"><img src="image/old sand pit.jpg" width="300" height="250" alt></td>
+		<td id="rtable"><img src="image/creek.jpg" width="300" height="250" alt></td>
+	</tr>
+	</table>
+	
+<p style="font-family:georgia,garamond,serif;color: white;"> Rearing poultry, cows, fishing and farming are just a few of the economic features that are promoted in this village. An independence monument is located in the heart of the village at the top if a sandy hill.</p>
+<br>
+<table style="width:100%;" border="0">
+	<tr>
+		<td id="rtable"><img src="image/independence monument.jpg" width="300" height="250" alt><td>
+		<td id="rtable"><img src="image/vege2.jpg" width="300" height="250" alt></td>
+		<td id="rtable"><img src="image/scene3.jpg" width="300" height="250" alt></td>
+	</tr>
+</table>
+
+<p style="font-family:georgia,garamond,serif;color: white;">Kuru Kururu has a rich untouched forest which is the home of much wild life such as wild cats, deers, wild cows, armadillos and many other wild animals. In Kuru Kururu there are many black water creeks where families spend day out swimming and having picnicking. Kuru Kururu is the perfect place for persons who love adventure and nature.<p>
+</center>
+
+<br>	
+	
+   </body>
+  
+  </tr>
+	</tbody>
+	</table>
+	</div>
+	</td>
+	</tr>
+	</tbody>
+	</table>
+	</td>
+	</tr>
+
+	<tr>
+   <td width="1000" valign="bottom"
+   
+   
+	  <p>True Vision Enterprises. All rights reserved. No part of this site may be reproduced without our written permission.</p>
+	  
+   <p id="demo"></p>
+   <script>
+document.getElementById("demo").innerHTML = Date();
+</script>
+   </footer>
+    </div>
+	  </td>
+	</tr>   
+   </body>
+
+
+</html>
---
+++ admin/home.php
@@ -0,0 +1,89 @@
+<html>
+<head>
+	<title>Home Page</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<style>
+	
+	 body {
+  margin: 0;
+  padding: 0;
+  background: #ccc;
+  background-image: url(image/);
+  background-repeat: no-repeat;
+  background-size: 100% 1250px;
+}
+	
+	 </style>
+	
+</head>
+
+<body>
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="index.php">Museum Homepage</a>
+	    </div>
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	         <li><a href=home.php>Home</a></li>
+	        <li><a href=accommodation.php>View Artifact</a></li>
+					<li><a href=tariff.php>TBD</a></li>
+					<li><a href=aboutus.php>About Us</a></li>
+	      </ul>
+				<ul class="nav navbar-nav navbar-right">
+        <li><a href="login.php">Login</a></li>
+      </ul>
+	    </div>
+	  </div>
+	</nav>
+	<br>
+	<br>
+
+	<div class="container-fluid" >
+		<img src="image/TVlogo.jpg" width=100% height=15%>
+		
+	</div>
+
+
+
+	<div class="container-fluid">
+		<div class =  "row">
+			<p></p>
+	
+			
+		</div>
+	</div>
+	<script src="js/bootstrap.min.js"></script>
+	
+	<table style="width:100%;" border="0">
+	
+	<tr>
+		<td id="rtable"><p style="color: black;font-size: 35px;text-align: center;"> ***Text For Museum to post here**</p>
+		</td>
+		</tr>
+		</table>
+		<br>
+		<table style="width:100%;" border="0">
+		<tr>
+		<td id="rtable"><p style="color: black;font-size: 35px;text-align: center;">***Text For Museum to post here**  </p>
+		</td>
+		</tr>
+	</table>
+	<br>
+	<br>
+	<br>
+ <footer>
+   <tr>
+   <td width="1000" valign="bottom">
+   
+	    <p>True Vision Enterprises. All rights reserved. No part of this site may be reproduced without our written permission.</p>
+	  
+   <p id="demo"></p>
+   <script>
+document.getElementById("demo").innerHTML = Date();
+</script>
+   </footer>
+</body>
+</html>
