2470f086325be44248e86925999795e3db48735c
rlweever <45017201+rlweever@users.noreply.github.com>
2018-11-14 10:41:41 -0400

    Add files via upload

---
+++ admin/portfolio.php
@@ -0,0 +1,64 @@
+<?php
+  session_start();
+?>
+<html>
+<head>
+  <title>National Museum</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	
+	<style>
+
+	body {
+  margin: 0;
+  padding: 0;
+  background: #ccc;
+  background-image: url(image/grey.png);
+  background-repeat: no-repeat;
+  background-size: 100% 1250px;
+}
+	
+	 </style>
+
+  <?php
+    include "connection.php";
+	include "index3.php";
+    	
+  ?>
+</head>
+  
+  <br>
+  <br>
+  <body>
+  
+  <?php
+    include "connection.php";
+	include "index3.php";
+   
+  ?>
+   
+  
+   <h2>Welcome <strong> <?php echo $_SESSION['username']; ?></strong> to the National Museum database. </h2>
+    
+
+
+  
+  
+  </body>
+	
+	
+	<br>
+  <footer>
+  <ul>
+  <li><strong><a href="logout.php">Logout</a></strong></li>
+  <br>
+   <li><strong><a href="reservation.php">Add Artifact</a></strong></li>
+   <br>
+   <li><strong><a href="history.php">Artifact History</a></strong></li>
+   </ul>
+   
+  </footer>
+  
+  
+
+</html>
